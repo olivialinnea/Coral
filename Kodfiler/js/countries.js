069b5c6bd1e90dfa6c919f03b4095f3484c0f610
murgrona <sawejonna@gmail.com>
2021-05-10 11:53:48 +0200

    Countrydiv-update

--- Kodfiler/js/countries.js
+++ Kodfiler/js/countries.js
@@ -1,18 +1,28 @@
 "use strict";
-
-
-function CountryName(name){
-    let countryDiv = document.createElement("div");
-    countryDiv.innerHTML = `
-    <div>${name}</div>
-    `;
-    countryDiv.classList.add("countryDiv");
-    document.querySelector("main").append(countryDiv);    
-};
+const wrapper = document.querySelector("wrapper");
 
 for(let i = 0; i < COUNTRIES.length; i++) {
-       CountryName(COUNTRIES[i].name) 
+    wrapper.append(CountryDiv(COUNTRIES[i]));
 }
 
+LANGUAGES.forEach(element => {
+    LANGUAGES.find(Language => element.countryID === Language.languageID);
+    wrapper.append(CountryDiv(LANGUAGES[i]));
+})
 
+function CountryDiv(data){
+    let country = document.createElement("div");
+    country.classList.add("country");
+
+    country.innerHTML = `
+    <img src="images/${data.spain_normal_1}.jpg">
+    <div>${data.name}</div>
+    <div id="langVisa">
+    <div>Language: ${data.languageID.name}</div>
+    <div>Visa requierd: ${data.visa}</div>
+    </div>
+    `;
+    
+    return country;
+}
 
