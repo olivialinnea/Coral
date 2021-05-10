be4ba1de45c85c7888f65b7bc8ac1c1b82654f3b
bea09bea <82158836+bea09bea@users.noreply.github.com>
2021-05-10 14:16:18 +0200

    accordion update

--- Kodfiler/js/universities.js
+++ Kodfiler/js/universities.js
@@ -6,9 +6,31 @@ function universityName(name){
     <div>${name}</div>
     `;
     universityDiv.classList.add("universityDiv");
+    let infoAbout = document.createElement("div");
+    infoAbout.innerHTML= `Information om land`
+    infoAbout.classList.add("panel");
+
+    document.querySelector("wrapper").append(infoAbout);  
     document.querySelector("wrapper").append(universityDiv);    
 };
 
 for(let i = 0; i < UNIVERSITIES.length; i++) {
     universityName(UNIVERSITIES[i].name) 
 }
+
+// Accordion
+let acc = document.getElementsByClassName("universityDiv");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+// Accordion
