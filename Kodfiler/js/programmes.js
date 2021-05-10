a378ecfae97f28dce37167ee7d72d4c41d142896
bea09bea <82158836+bea09bea@users.noreply.github.com>
2021-05-10 14:01:27 +0200

    accordion

--- Kodfiler/js/programmes.js
+++ Kodfiler/js/programmes.js
@@ -6,9 +6,33 @@ function programmeName(name){
     <div>${name}</div>
     `;
     programmeDiv.classList.add("programmeDiv");
+
+    let infoAbout = document.createElement("div");
+    infoAbout.innerHTML= `Information om land`
+    infoAbout.classList.add("panel");
+
+    document.querySelector("wrapper").append(infoAbout);  
     document.querySelector("wrapper").append(programmeDiv);    
 };
 
 for(let i = 0; i < PROGRAMMES.length; i++) {
     programmeName(PROGRAMMES[i].name) 
 }
+
+
+// Accordion
+let acc = document.getElementsByClassName("programmeDiv");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+// Accordion
