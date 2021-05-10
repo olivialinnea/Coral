dfeb0c005cc71b9a0a7ec8bd821a658e8acb2d11
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2021-05-10 11:55:03 +0200

    added to top button

---
+++ Kodfiler/js/global.js
@@ -0,0 +1,29 @@
+"use strict";
+
+goToTopButton();
+
+function goToTopButton() {
+    let button = document.createElement("BUTTON");
+
+    button.innerHTML = `^`
+    button.classList.add("topButton");
+    document.querySelector("main").append(button);
+    
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          button.style.display = "block";
+        } else {
+          button.style.display = "none";
+        }
+    }
+
+    button.addEventListener("click", () => {
+        document.documentElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    });
+
+}
