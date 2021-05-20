3ed77c181c7dc4a1d8bd028174ad85be873d2833
bea09bea <82158836+bea09bea@users.noreply.github.com>
2021-05-20 18:15:37 +0200

    sideInfo fixed

--- Kodfiler/js/universities.js
+++ Kodfiler/js/universities.js
@@ -4,6 +4,17 @@
 
 const wrapper = document.querySelector("wrapper");
 
+let yearArray = [];
+let D = new Date();
+let thisYear = D.getFullYear();
+
+for (let i = 0; i < 5; i++) {
+  thisYear -=1;
+  yearArray.push(thisYear);
+}
+
+yearArray.sort();
+
 const searchBar = document.getElementById("uniSearch");
 let uniNames = UNIVERSITIES;
 
@@ -35,16 +46,29 @@ for(let i = 0; i < UNIVERSITIES.length; i++) {
 function universityDiv(data){
   let universityDiven = document.createElement("div");
   universityDiven.classList.add("universityDiv");
+
+  const city = CITIES.find(cities => data.cityID === cities.id);
+  const c = COUNTRIES.find(function (c) {
+    if (city.countryID === c.id) {
+      let b = CITIES.find(function (city) {
+        if (data.cityID === city.id) {
+          return city;
+        }
+      });
+      return b;
+    }
+  });
+  
   
   universityDiven.innerHTML = `
   <div class="top"> 
     <div>${data.name}</div>
 
   <div class="universitySideInfo"> 
-      <img class="countryFlag" src="Images/">
+      <img class="countryFlag" src="Images/${c.flag}">
       <div class="position" >
-        <div> Land: </div>
-        <div> Stad: </div>
+        <div> Land: ${c.name} </div>
+        <div> Stad: ${city.name} </div>
       </div>
     </div>
   </div>
@@ -130,8 +154,90 @@ function infoDivUnder(data) {
       
     });
 
+
+    let gradesDiv = document.createElement("div");  
+    let entryGradeDiv = document.createElement("div");
+    let graduatesDiv = document.createElement("div");
+    let yearDiv = document.createElement("div");
+    let procentageDiv = document.createElement("div");
+    let graduatesHead = document.createElement("div");
+    let entryHead = document.createElement("div");
+    let headerContainer = document.createElement("div");
+    let entryDiv = document.createElement("div");
+    let entryYearDiv = document.createElement("div");
+    
+    graduatesHead.classList.add("graduatesHead");
+    graduatesDiv.classList.add("graduates");
+    entryGradeDiv.classList.add("grades");
+    gradesDiv.classList.add("gradesDiv");
+    entryHead.classList.add("entryHead");
+    entryDiv.classList.add("entryDiv");
+    entryYearDiv.classList.add("entryYearDiv");
+    headerContainer.classList.add("headerContainer");
+  
+    headerContainer.append(entryHead, graduatesHead);
+    gradesDiv.append(entryGradeDiv, graduatesDiv);
+    graduatesDiv.append(yearDiv, procentageDiv);
+    entryGradeDiv.append(entryYearDiv, entryDiv);
+  
+    entryHead.textContent = "Intagningsbetyg"
+    graduatesHead.textContent = "Antal examinerade"
+    let successRate = program[j].successRate;
+    let entryGrades = program[j].entryGrades;
+
+    
+  let reviewDiv = document.createElement("div");
+  let reviewHead = document.createElement("div");
+  let reviews = document.createElement("div");
+  reviews.classList.add("reviews");
+  reviewHead.classList.add("reviewHeader");
+  reviewDiv.classList.add("reviewContent");
+
+  reviewHead.textContent = "Omdömen från före detta studenter";
+
+  reviewDiv.append(reviewHead, reviews);
+  
+    yearArray.forEach(function(year) {
+      yearDiv.append(yearlyAmount(year))
+      entryYearDiv.append(yearlyAmount(year))
+    });
+  
+    successRate.forEach(function(rate) {
+      procentageDiv.append(procentageKey(rate))
+    });
+    
+    entryGrades.forEach(function(grade) {
+      entryDiv.append(gradesKey(grade))
+    });
+
+    idDiv.append(headerContainer, gradesDiv, reviewDiv);
   }
+
+  
 
   return infoAbout;
 }
 
+function yearlyAmount(year){
+  let div = document.createElement("div");
+
+  div.textContent = `${year}`;
+  
+  return div;
+}
+
+function procentageKey(key){
+  let div = document.createElement("div");
+
+  div.textContent = `${key}%`;
+  
+  return div;
+}
+
+function gradesKey(key){
+  let div = document.createElement("div");
+
+  div.textContent = `${key}`;
+  
+  return div;
+}
